c9319dee9fb2ac5006d829361c391d3f2a1c7972
mygithub2211 <phattran13062002@gmail.com>
2024-06-03 01:32:35 -0400

    Commit

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 import MainPage from './components/MainPage/MainPage';
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import DetailsPage from './components/DetailsPage/DetailsPage';
 import Navbar from './components/Navbar/Navbar';
+
 function App() {
   const navigate = useNavigate();
   return (
@@ -16,6 +18,7 @@ function App() {
           <Route path="/app" element={<MainPage />} />
           <Route path="/app/login" element={<LoginPage/>} />
           <Route path="/app/register" element={<RegisterPage />} />
+          <Route path="/app/product/:productId" element={<DetailsPage/>} />
         </Routes>
         </>
   );
